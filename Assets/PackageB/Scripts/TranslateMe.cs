978261851c0be0676f4a4e1793205ad1162660ff
Jinjiacheng <jinjiacheng@sensetime.com>
2020-05-20 20:37:18 +0800

    update

---
+++ Assets/PackageB/Scripts/TranslateMe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateMe : MonoBehaviour
+{
+    public float angularVelocity = 10;
+   void Update ()
+   {
+       var rot = Quaternion.AngleAxis (angularVelocity * Time.deltaTime, Vector3.forward);
+       transform.localRotation = rot * transform.localRotation;
+   }
+}
